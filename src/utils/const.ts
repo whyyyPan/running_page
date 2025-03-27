001fa2a4038e42840cdf8bd7d2de039145bdce72
Pan <pan@elifeteam.com>
2025-03-27 19:42:10 +0800

    update map height

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -35,7 +35,7 @@ const USE_DASH_LINE = true;
 // styling: route line opacity: [0, 1]
 const LINE_OPACITY = 0.4;
 // styling: map height
-const MAP_HEIGHT = 600;
+const MAP_HEIGHT = 1000;
 //set to `false` if you want to hide the road label characters
 const ROAD_LABEL_DISPLAY = false;
 // update for now 2024/11/17 the privacy mode is true
