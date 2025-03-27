15790b1762e504e97420169d8f69b67979f0bdcf
Pan <pan@elifeteam.com>
2025-03-27 19:23:49 +0800

    update ROAD_LABEL_DISPLAY

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -37,7 +37,7 @@ const LINE_OPACITY = 0.4;
 // styling: map height
 const MAP_HEIGHT = 600;
 //set to `false` if you want to hide the road label characters
-const ROAD_LABEL_DISPLAY = true;
+const ROAD_LABEL_DISPLAY = false;
 // update for now 2024/11/17 the privacy mode is true
 //set to `true` if you want to display only the routes without showing the map.
 const PRIVACY_MODE = false;
